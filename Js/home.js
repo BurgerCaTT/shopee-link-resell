106e494782e5a254b7126535b6e612b4882d0a0e
BurgerCaTT <11giakhanh03@gmail.com>
2025-12-19 11:04:53 +0700

    test link

--- Js/home.js
+++ Js/home.js
@@ -1,7 +1,7 @@
-const API_BASE =
-  location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "https://shopeelinks-api.onrender.com/api";
+// const API_BASE =
+//   location.hostname === "localhost"
+//     ? "http://localhost:5000/api"
+//     : "https://shopeelinks-api.onrender.com/api";
 
 
 
