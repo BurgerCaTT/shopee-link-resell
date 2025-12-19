eed36d3b43dca7d5aa6f68cbf590dab70f620088
BurgerCaTT <11giakhanh03@gmail.com>
2025-12-19 11:02:26 +0700

    test link shopee

--- Js/home.js
+++ Js/home.js
@@ -13,30 +13,30 @@ window.addEventListener("load", () => {
 });
 
 // ===== LOAD CATEGORIES =====
-fetch(`${API_BASE}/categories`)
-  .then(res => res.json())
-  .then(data => {
-    const menu = document.getElementById("categoryMenu");
-    const list = document.getElementById("categoryList");
+// fetch(`${API_BASE}/categories`)
+//   .then(res => res.json())
+//   .then(data => {
+//     const menu = document.getElementById("categoryMenu");
+//     const list = document.getElementById("categoryList");
 
-    menu.innerHTML = "";
-    list.innerHTML = "";
+//     menu.innerHTML = "";
+//     list.innerHTML = "";
 
-    data.forEach(c => {
-      // Dropdown menu
-      const li = document.createElement("li");
-      li.innerHTML = `<a class="dropdown-item" href="category.html?slug=${c.slug}">${c.name}</a>`;
-      menu.appendChild(li);
+//     data.forEach(c => {
+//       // Dropdown menu
+//       const li = document.createElement("li");
+//       li.innerHTML = `<a class="dropdown-item" href="category.html?slug=${c.slug}">${c.name}</a>`;
+//       menu.appendChild(li);
 
-      // List group
-      const a = document.createElement("a");
-      a.className = "list-group-item list-group-item-action";
-      a.href = `category.html?slug=${c.slug}`;
-      a.innerText = c.name;
-      list.appendChild(a);
-    });
-  })
-  .catch(() => {
-    document.getElementById("categoryList").innerHTML =
-      "<div class='text-danger'>Không tải được danh mục</div>";
-  });
+//       // List group
+//       const a = document.createElement("a");
+//       a.className = "list-group-item list-group-item-action";
+//       a.href = `category.html?slug=${c.slug}`;
+//       a.innerText = c.name;
+//       list.appendChild(a);
+//     });
+//   })
+//   .catch(() => {
+//     document.getElementById("categoryList").innerHTML =
+//       "<div class='text-danger'>Không tải được danh mục</div>";
+//   });
